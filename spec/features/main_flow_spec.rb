8b4b813ecca181e322044100e0513992543b3091
Molly Trombley-McCann <molly.kayline@gmail.com>
2021-01-18 10:18:49 -0800

    Hosts index and show pages (#5)
    [#176430029,#176309697]

--- spec/features/main_flow_spec.rb
+++ spec/features/main_flow_spec.rb
@@ -4,9 +4,10 @@ describe 'viewing recommendations' do
   before do
     episode1 = FactoryBot.create(:episode, number: 1, release_date: DateTime.new(2020, 11, 1))
     episode2 = FactoryBot.create(:episode, number: 2, release_date: DateTime.new(2020, 11, 15))
-    greg = FactoryBot.create(:person, first_name: 'Thirst', last_name: 'von Trapp')
-    FactoryBot.create(:recommendation, name: 'Teenage Mutant Ninja Turtles', episode: episode1)
-    FactoryBot.create(:recommendation, name: 'Pacific Rim', person: greg, episode: episode2)
+    thirst = FactoryBot.create(:person, first_name: 'Thirst', last_name: 'von Trapp')
+    queen = FactoryBot.create(:person, first_name: 'Midwest', last_name: 'Queen')
+    FactoryBot.create(:recommendation, name: 'Teenage Mutant Ninja Turtles', person: queen, episode: episode1)
+    FactoryBot.create(:recommendation, name: 'Pacific Rim', person: thirst, episode: episode2)
   end
 
   it 'displays an ordered list of all recommendations and shows the details for one' do
@@ -16,12 +17,22 @@ describe 'viewing recommendations' do
     titles = page.all('.recommendation-title').map {|node| node.text}
     expect(titles).to eq([
                               'Pacific Rim - Thirst von Trapp',
-                              'Teenage Mutant Ninja Turtles - Christmas Zaddy'
+                              'Teenage Mutant Ninja Turtles - Midwest Queen'
                             ])
     click_on 'Pacific Rim'
     expect(page).to have_content('Thirst von Trapp')
     expect(page).to have_content('Episode 2')
     click_on 'Who Shot Ya Recommends'
     expect(page).to have_content('Recommendations')
+  end
+
+  it 'shows a list of hosts and the recommendations for each' do
+    visit '/'
+    click_on 'Recs By Host'
+    expect(page).to have_content('Thirst von Trapp')
+    expect(page).to have_content('Midwest Queen')
+    click_on 'Thirst von Trapp'
+    expect(page).to have_content('Pacific Rim')
+    expect(page).not_to have_content('Teenage Mutant Ninja Turtles')
   end
 end
